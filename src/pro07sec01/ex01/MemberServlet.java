0f0681af11aff07d5df6438d1f321460f17582f2
bc0086 <bc0086@naver.com>
2020-12-11 00:15:31 +0900

    2020-12-11 00:15 h

--- src/pro07sec01/ex01/MemberServlet.java
+++ src/pro07sec01/ex01/MemberServlet.java
@@ -2,6 +2,7 @@ package pro07sec01.ex01;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -11,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/member")
-protected class MemberServlet extends HttpServlet {
+public class MemberServlet extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=utf-8");
@@ -26,14 +27,14 @@ protected class MemberServlet extends HttpServlet {
 		out.print("<table border=1><tr align='center' bgcolor = 'lightgreen'>");
 		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td></tr>");
 		
-		// 조회한 회원 정보를 for문솨 <tr>태그를 이용해 리스트로 출력
+		// 조회한 회원 정보를 for문과 <tr>태그를 이용해 리스트로 출력
 		for(int i =0; i<list.size(); i++) {
 			MemberVO memberVO = list.get(i);
 			String id = memberVO.getId();
 			String pwd = memberVO.getPwd();
 			String name = memberVO.getName();
 			String email = memberVO.getEmail();
-			String joinDate = memberVO.getJoinDate();
+			Date joinDate = memberVO.getJoinDate();
 			out.print("<tr><td>" + id + " </td><td>" + pwd + "</td><td>" + name + "</td><td>" 
 					+ email + "</td><td>" + joinDate + "</td></tr>");
 		}
